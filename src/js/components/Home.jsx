0856bb5c9d23f5e9867db647ae5ebececc1a94e2
ManuelFMG <manuel_psp86@hotmail.com>
2026-01-03 10:18:54 +0000

    Lista de tareas con fetch actualizada

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+//INICIADO CON PATRICIA
 const Home = () => {
   const API_URL = "https://playground.4geeks.com/todo";
   const USER = "Manuel";
@@ -56,6 +56,19 @@ const Home = () => {
       headers: {
         "Content-Type": "application/json",
       },
+    });
+    .then(resp => {
+        console.log(resp.ok); // Será true si la respuesta es exitosa
+        console.log(resp.status); // El código de estado 201, 300, 400, etc.
+        return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+    })
+    .then(data => {
+        // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+    })
+    .catch(error => {
+        // Manejo de errores
+        console.log(error);
     });
     getUser(); // refresca la lista
   };
